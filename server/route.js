f3598a0b85dfad24a41baca623d2efa79e2e9540
FredSchuck <fredschuck@gmail.com>
2025-01-30 07:39:03 -0500

    Created simple route for testing

---
+++ server/route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as controller from "controller.js";
+
+const router = express.Router();
+
+router.post("/calc", controller.calculate);
+
+export default router;
