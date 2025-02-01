9f508a388ccb8b21fb535a1a816927966d6ad317
FredSchuck <fredschuck@gmail.com>
2025-02-01 11:53:21 -0500

    Changed proxy url pointer

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://main.dbjc9749ps6vl.amplifyapp.com",
     },
   },
 });
