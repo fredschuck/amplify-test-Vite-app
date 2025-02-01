1f1969737c1c823ed092fbfd787c6f03724c7cd6
FredSchuck <fredschuck@gmail.com>
2025-02-01 14:23:09 -0500

    Loosened cors rules

--- server/server.js
+++ server/server.js
@@ -14,13 +14,14 @@ const app = express();
 
 // CORS
 app.use(cors());
-app.use(
-  cors({
-    origin: process.env.API_URL || "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+app.use(cors({ origin: "*" }));
+// app.use(
+//   cors({
+//     origin: process.env.API_URL || "http://localhost:3000",
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
